50ba015f417135f4ed4253197e5a0283f3dac4cf
Raul Bunicenha <rbunicenha@hotmail.com>
2013-05-02 13:22:04 -0300

    Tela Contas a receber 2

--- php/financeiro/contas_a_receber/tela.php
+++ php/financeiro/contas_a_receber/tela.php
@@ -34,45 +34,41 @@
 	div#contas_a_receber{
 		padding:0 0 0 0;	
 	}
-	
 	.Fundo_total{
-		width:99%; height:100%; background:#F5F5F5; margin:auto;
+		width:100%; height:100%; background:#E4E4E4; margin:auto;
 	}
-	
 	.Filtros_top{
-		width:100%;text-align:left; 
+		width:100%;text-align:left;
 	}
-	
 	Filtros_top_form{
 		margin:5px 5px 5px 5px;	
 	}
-	
 	#ConRec_descricao{
 		width:400px;	
 	}
-	
 	#ConRec_select_campo{
 		width:250px;
 	}
-	
 	select[name^='ConRec_select_data']{
 		width:100px;	
 	}
-	
 	.Contas{
 		height:60%; width:99%; overflow:auto; border:1px solid #000; margin:auto; margin-top:2px;
 	}
-	
 	.Func_botton1, .Func_botton3{
-		width:99%; height:5%; margin:auto; margin-top:2px;
+		width:100%; height:5%; margin-top:1px; float:left;
 	}
-	
 	.Func_botton2{
-		width:99%; height:10%; margin:auto; margin-top:2px;	
+		width:100%; height:10%; margin-top:2px; float:left;	
 	}
-	
-	input:button[name^='ConRec_bt_']{
-		padding: 10px 10px 10px 10px;
+	#ConRec_quantidade, #ConRec_ttselecionado{
+		width:60px;	
+	}
+	input[id^='ConRec_bt']{
+		float:left; padding:5px 5px 5px 5px; margin-right:10px;
+	}
+	#ConRec_bt_baixar{
+		float:right;	
 	}
 </style>
 
@@ -119,21 +115,29 @@
         <!----FORMULARIO INFERIOR1----->
         <div class="Func_botton1">
         
-        	<input type="button" name="ConRec_bt_desconto" value="Desconto" />
-        	<input type="button" name="ConRec_bt_lanc_cheque" value="Lançamento Cheque" />
-           	<input type="button" name="ConRec_bt_rec_cheque"  value="Recebimento Cheque" />
+       	  <input type="button" name="ConRec_bt_desconto" id="ConRec_bt_desconto" value="Desconto" />
+       	  <input type="button" name="ConRec_bt_lanCheque" id="ConRec_bt_lanCheque" value="Lançamento Cheque" />
+       	  <input type="button" name="ConRec_bt_recCheque" id="ConRec_bt_recCheque" value="Recebimento Cheque" />
             Quantidade:
-            <input type="text" 	 name="ConRec_quantidade"      disabled="disabled"	 value="<?php  ?>" />
+          <input type="text" name="ConRec_quantidade" disabled="disabled"	id="ConRec_quantidade" value="<?php  ?>" />
             Total Selecionado:
-            <input type="text" 	 name="ConRec_tt_selecionado"  disabled="disabled" 	 value="<?php  ?>" />
-           	<input type="button" name="ConRec_bt_baixar"  value="Baixar" />
-
+          <input type="text" name="ConRec_ttselecionado" disabled="disabled" id="ConRec_ttselecionado" value="<?php  ?>" />
+          <input type="button" name="ConRec_bt_baixar" id="ConRec_bt_baixar" value="Baixar" />
+          
         </div>
         <!---------------------------->
-        
         <!----FORMULARIO INFERIOR2----->
         <div class="Func_botton2">
         
+        	<fieldset>
+            	<legend><strong> Informação do Documento </strong> </legend>
+                	Numero: 
+                    <input type="text" name="ConRec_numero" id="ConRec_numero"/>
+                    Venda/Representante:
+                    <input type="text" name="ConRec_venda" id="ConRec_venda"/>
+                    <input type="text" name="ConRec_nome" id="ConRec_nome"/>
+            </fieldset>
+            
         </div>
         <!---------------------------->
     
@@ -141,8 +145,15 @@
         <!----FORMULARIO INFERIOR3----->
         <div class="Func_botton3">
         
+        	<fieldset>
+            	<legend><strong> Status </strong></legend>
+                <input type="checkbox" value="aberto/parcial" name="ConRec_ck1" /> Aberto/Parcial
+                <input type="checkbox" value="baixa_total" name="ConRec_ck2" /> Baixa Total
+                <input type="checkbox" value="adiantamento" name="ConRec_ck3" /> Adiantamento
+                <input type="checkbox" value="todos" name="ConRec_ck4" /> Todos
+            </fieldset>
+            
         </div>
         <!---------------------------->
-	</fieldset>
     
 </div>
